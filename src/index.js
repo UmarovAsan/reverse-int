33151509014622d8385db1737b2843cbed83a8db
asan-31@mail.ru <asan-31@mail.ru>
2023-10-11 17:42:35 +0600

    feat: add function reverse for reverse value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let isNegative = n < 0;
+    if (isNegative) {
+        n = Math.abs(n);
+    }
+    let reversed = parseInt(n.toString().split("").reverse().join(''));
+    console.log(reversed);
+    return isNegative ? reversed : reversed;
+};
